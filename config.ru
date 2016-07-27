37b4532745dc8c640aeaa687fe9a45daa3e31135
NG <ngsaip7@gmail.com>
2016-07-27 09:33:09 +0530

    removed config.ru

--- config.ru
+++
@@ -1,9 +0,0 @@
-$:.unshift File.expand_path("./../lib", __FILE__)
-
-require 'bundler'
-Bundler.require
-
-require './config/environment'
-
-# connection managment so that connection doesn't stay open when requests are finished
-use ActiveRecord::ConnectionAdapters::ConnectionManagement
--- config/environment.rb
+++ config/environment.rb
@@ -1,11 +1,8 @@
 require 'yaml'
 require 'grpc'
-require 'bundler'
-Bundler.require
 
 Dir["./models/*.rb"].each {|file| require file }
-#Dir["./lib/*.rb"].each {|file| require file }
-#require_relative "../users_api_server"
+Dir["./lib/*.rb"].each {|file| require file }
 
 environment = ENV.fetch('RACK_ENV') { 'development' }
 db_options = YAML.load(File.read('./config/database.yml'))[environment]
