cc3d095611dacfa0f57fdf0fc442bd7e3af1df17
NG <ngsaip7@gmail.com>
2016-07-28 10:22:27 +0530

    added timestamps

--- db/migrate/0_create_users.rb
+++ db/migrate/0_create_users.rb
@@ -4,6 +4,8 @@ class CreateUsers < ActiveRecord::Migration
       t.string :first_name
       t.string :last_name
       t.string :email
+
+      t.timestamps
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -13,10 +13,12 @@
 ActiveRecord::Schema.define(version: 1) do
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "email"
-    t.string "phone"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "phone"
   end
 
 end
